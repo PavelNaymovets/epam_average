fd4b2732100bcc36433e5a0c4e2e1eaaf0a0be60
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-05-26 23:46:10 +0300

    Answer for third task

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,17 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        String[] str = scanner.nextLine().split(" ");
+        int arr = 0;
+        for (int i = 0; i < str.length; i++) {
+            arr += Integer.parseInt(str[i]);
+        }
+        System.out.println(arr/(str.length - 1));
     }
 
 }
